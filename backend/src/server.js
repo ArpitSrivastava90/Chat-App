222373cd698920aeaa85e2d11101638a3916aad9
Arpit <arpitsrivastav2023@gmail.com>
2025-10-04 21:16:27 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import AuthRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const __dirname = path.resolve(); // C:\Web development\Projects - 1\chatapp\backend
+
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", AuthRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Your server is running on port ${PORT}`);
