8f96d018e6416c3d3fb2796b0221dc0d30b4d8e3
Arpit <arpitsrivastav2023@gmail.com>
2025-10-08 19:45:51 +0530

    Added send msg

--- backend/src/controllers/send.controllers.js
+++ backend/src/controllers/send.controllers.js
@@ -38,3 +38,32 @@ export const getMessageByUserId = async (req, res) => {
     res.status(500).json("Server Error");
   }
 };
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    //todo : need to setup RTC
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage controllers ", error.message);
+    res.status(500).json("Server Error");
+  }
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getAllContacts,
   getMessageByUserId,
+  sendMessage,
 } from "../controllers/send.controllers.js";
 import { Middelware } from "../lib/utlis.js";
 const router = express.Router();
@@ -10,6 +11,6 @@ router.get("/contacts", Middelware, getAllContacts);
 // router.get("/chats", getChatPatners);
 router.get("/:id", Middelware, getMessageByUserId);
 
-// router.("/send/:id", Middelware, sendMessage);
+router.post("/send/:id", Middelware, sendMessage);
 
 export default router;
